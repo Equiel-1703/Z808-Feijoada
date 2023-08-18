29ebd0277f6bf7549c72604fe484777ef5d8637a
Henrique Rodrigues <96885946+Equiel-1703@users.noreply.github.com>
2023-08-18 02:05:18 -0300

    Started refactoring

--- src/main/java/macroprocessor/MacroProcessor.java
+++ src/main/java/macroprocessor/MacroProcessor.java
@@ -4,57 +4,94 @@ import assembler.Log;
 import assembler.LogType;
 import assembler.Logger;
 import macroprocessor.macrotable.Macro;
-import macroprocessor.macrotable.MacroTable;
 import assembler.utils.AssemblerUtils;
 
 import java.io.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.stream.Collectors;
 
 public class MacroProcessor {
     // Macro processor
-    private final MacroTable macroTable;
+    private final HashMap<String, Macro> macroTable;
     private boolean isInsideMacroDefinition = false;
-    private int macroAlignmentLevel = 0;
+    private int currentMacroAlignmentLevel = 0;
+    private boolean hasNestedMacros = false;
 
     // New file output lines for assembler
-    private final LinkedList<String> newFileLines = new LinkedList<>();
+    private final LinkedList<String> step1Lines;
+    private final LinkedList<String> step2Lines;
 
     // Handling files utils
     private String currentLine;
     private int lineCounter;
-    private LinkedList<String> lines;
 
     // Macros defines
     private static final String MACRODEF = "MACRODEF";
     private static final String MACROEND = "ENDM";
 
     public MacroProcessor() {
-        this.macroTable = MacroTable.getInstance();
+        macroTable = new HashMap<>();
+        step1Lines = new LinkedList<>();
+        step2Lines = new LinkedList<>();
     }
 
     public String parseMacros(String pathToProgram) throws Exception {
-        FileReader fileReader = new FileReader(pathToProgram);
+        lineCounter = 0;
         Logger.getInstance().reset();
 
-        // Parse line by line
-        try (BufferedReader fileIO = new BufferedReader(fileReader)) {
+        // Read entire file to linked list ignoring comments and blank lines
+        try {
+            FileReader fileReader = new FileReader(pathToProgram);
+            BufferedReader fileIO = new BufferedReader(fileReader);
+
             for (String s : fileIO.lines().toList()) {
+                s = s.trim();
                 if (!s.isEmpty()) {
                     if (s.trim().charAt(0) != ';') {
-                        this.lines.add(s);
+                        this.step1Lines.add(s);
                     }
                 }
             }
 
-            while (lineCounter < lines.length && (this.currentLine = lines.get(this.lineCounter)) != null) {
-                // Check for macros
-                parseLine();
-                // Next Line
-                lineCounter += 1;
-            }
+            fileReader.close();
+            fileIO.close();
+
+            // Executes passes
+            do {
+                // Step 1: macro declaration processing
+                while (lineCounter < step1Lines.size()) {
+                    // Reads first line from list
+                    currentLine = step1Lines.pop();
+
+                    // Check for macros
+                    parseMacroDeclaration();
+
+                    // Next Line
+                    lineCounter += 1;
+                }
+
+                lineCounter = 0;
+                step1Lines.clear();
+
+                // Step 2: macro expansion
+                while (lineCounter < step2Lines.size()) {
+                    // Reads first line from list
+                    currentLine = step2Lines.pop();
+
+                    // Check for macros
+                    parseMacroExpansion();
+
+                    // Next Line
+                    lineCounter += 1;
+                }
+
+            } while (hasNestedMacros);
+
+            // If no nested macros were found, then the macro expansion is done and the
+            // final extended code is in step1Lines
+
         } catch (IOException e) {
             Logger.getInstance().addLog(new Log(LogType.ERROR, lineCounter, "Error while reading " + pathToProgram + "file: " + e.getMessage()));
             Logger.getInstance().printLogs();
@@ -64,10 +101,10 @@ public class MacroProcessor {
         // Removes ".asm" extension and append ".pre"
         pathToProgram = pathToProgram.replace(".asm", ".pre");
 
-        // Writing intermediate file
+        // Writing intermediate file (this will be sent to Assembler)
         OutputStream outputStream = new FileOutputStream(pathToProgram);
         try (DataOutputStream dataOutStream = new DataOutputStream(outputStream)) {
-            for (String line : newFileLines)
+            for (String line : step1Lines)
                 dataOutStream.writeBytes(line + "\n");
         } catch (IOException e) {
             Logger.getInstance().addLog(new Log(LogType.ERROR, lineCounter, "Error while writing " + pathToProgram + "file: " + e.getMessage()));
@@ -76,24 +113,25 @@ public class MacroProcessor {
         }
 
         // Log success message
-        Logger.getInstance().addLog(new Log(LogType.INFO, 0, "Macros finished to be replaced!"));
+        Logger.getInstance().addLog(new Log(LogType.INFO, 0, "Macros expanded!"));
         Logger.getInstance().printLogs();
 
         return pathToProgram;
     }
 
-    public boolean parseLine() throws Exception {
+    public void parseMacroDeclaration() throws Exception {
         // All tokens in line
         String[] tokens = AssemblerUtils.decomposeInTokens(this.currentLine);
 
-        // is macro declaration?
-        if (tokens[1].equalsIgnoreCase(MACRODEF))
-            return false;
+        // is external macro declaration?
+        if (tokens[1].equalsIgnoreCase(MACRODEF) && !isInsideMacroDefinition) {
+
+
+        }
 
         // Macro name is valid?
         if (!AssemblerUtils.isValidMacro(this.currentLine)) {
             throw new Exception("Invalid macro name");
-            return false;
         }
 
         // Macro name
@@ -121,19 +159,19 @@ public class MacroProcessor {
 
             int startMacroDef = lineCounter;
             StringBuilder macroCode = new StringBuilder();
-            while (!lines[lineCounter].equals(MACROEND)) {
-                this.currentLine = lines[lineCounter];
+            while (!step2Lines[lineCounter].equals(MACROEND)) {
+                this.currentLine = step2Lines[lineCounter];
 
                 // Check if it's a nested macro definition
                 if (this.currentLine.equalsIgnoreCase(MACRODEF)) {
                     this.macroAlignmentLevel += 1;
                     try {
-                        parseLine();
+                        parseMacroDeclaration();
                     } catch (StackOverflowError e) {
                         Logger.getInstance().addLog(new Log(LogType.ERROR, lineCounter, "Maximum nested macro level reached!"));
                     }
                 }
-                macroCode.append(lines[lineCounter]).append("\n");
+                macroCode.append(step2Lines[lineCounter]).append("\n");
                 lineCounter += 1;
             }
             int endMacroDef = lineCounter;
@@ -154,10 +192,10 @@ public class MacroProcessor {
         this.macroTable.reset();
         this.isInsideMacroDefinition = false;
         // New file output lines for assembler
-        this.newFileLines.clear();
+        this.step1Lines.clear();
         // Handling files utils
         this.currentLine = "";
-        this.lines.clear();
+        this.step2Lines.clear();
         // Gay é o henrique
         this.lineCounter = 0;
     }
